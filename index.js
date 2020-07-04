5580c9b012faed0795d15e4a697d5835cc10bce6
bgh716 <bgh716@naver.com>
2020-07-04 02:15:51 -0600

    working on login using session

--- index.js
+++ index.js
@@ -3,14 +3,27 @@ const session = require('express-session');
 const bodyParser = require('body-parser');
 const path = require('path')
 const PORT = process.env.PORT || 5000
-
+const Psession = require('connect-pg-simple')(session);
 const { Pool } = require('pg');
 var pool;
 pool = new Pool({
-    connectionString:process.env.DATABASE_URL
+    connectionString:'postgres://postgres:SFU716!!qusrlgus@localhost/users'
+    //connectionString:process.env.DATABASE_URL
 })
 
 var app = express();
+app.use(session({
+    store: new Psession({
+        
+        conString:'postgres://postgres:SFU716!!qusrlgus@localhost/postgres'
+        //connectionString:process.env.FOO_COOKIE_SECRET
+
+    }),
+    secret: '!@SDF$@#SDF',
+    resave: false,
+    cookie:{ maxAge: 30 * 24 * 60 * 60 * 1000 },
+    saveUninitialized: true
+}));
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
@@ -19,38 +32,50 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.get('/', (req, res) => res.render('pages/index'));
 
-app.post('/auth/login', function(req, res){
+app.post('/auth/login', (req, res) =>{
     var uid = req.body.uid;
     var upassword = req.body.upassword;
-    if(uid!=null && upassword!=null){
-        pool.query('SELECT * FROM backpack WHERE uid=$1 OR upassword=$2', [uid], [upassword] , (error,result)=>{
+    var values=[uid, upassword];
+    if(uid && upassword){
+        pool.query('SELECT * FROM backpack WHERE uid=$1 AND upassword=$2', values , (error,result)=>{
             if(error)
                 res.end(error);
-            else if(result&&result.rows[0]){
-                res.send(uid);
+            else if(!result||!result.rows[0]){
+                res.send(`Who are  you?`);
+            }
+            else{
+                req.session.displayName = result.rows[0].uname;
+                res.send(`
+                    <h1>hello, ${req.session.displayName} </h1>
+                    <a href="/auth/logout">logout</a>
+                    `);
             }
         });
     }
 });
 
-
+app.get('/auth/logout', (req, res)=>{
+    delete req.session.displayName;
+    res.send(`log out`);
+});
 
 app.post('/adduser', (req, res) => {
     var uid = req.body.uid;
     var uname = req.body.uname;
     var uemail = req.body.uemail;
     var upassword = req.body.upassword;
+    var checking = [uid, uemail];
     var values=[uid, uname, uemail, upassword];
     if(uid && uname && uemail && upassword){
-        pool.query('SELECT * FROM backpack WHERE uid=$1 OR uemail=$2', [uid], [uemail] , (error,result)=>{
-            if(error)
+        pool.query('SELECT * FROM backpack WHERE uid=$1 OR uemail=$2', checking , (error,result)=>{
+            if(error){
                 res.end(error);
+            }
             else if(result&&result.rows[0]){
                 res.send(`USER ID or EMAIL is already taken!`);
             }
             else{
-                var insertUsersQuery=`INSERT INTO backpack (uid, uname, uemail, upassword) VALUES ($1,$2,$3,$4)`
-                pool.query(insertUsersQuery,values, (error,result)=>{
+                pool.query(`INSERT INTO backpack (uid, uname, uemail, upassword) VALUES ($1,$2,$3,$4)`,values, (error,result)=>{
                     if(error)
                         res.end(error);
                     else{
@@ -66,16 +91,18 @@ app.post('/adduser', (req, res) => {
 app.post('/deleteuser', (req, res) => {
     var uid = req.body.uid;
     var upassword = req.body.upassword;
+    var checking =[uid, upassword]
     if(uid && upassword){
-        pool.query('SELECT * FROM backpack WHERE uid=$1 AND upassword=$2', [uid], [upassword], (error,result)=>{
+        pool.query('SELECT * FROM backpack WHERE uid=$1 AND upassword=$2', checking, (error,result)=>{
             if(error)
                 res.end(error);
-            else if(result&&result.rows[0]){
+            else if(!result||!result.rows[0]){
                 res.send(`USER ID or PASSWORD is not correct!`);
             }
             else{
-                var insertUsersQuery=`DELETE FROM usr WHERE id=$1`
-                pool.query(insertUsersQuery,values, (error,result)=>{
+
+                var insertUsersQuery=`DELETE FROM backpack WHERE uid=$1`
+                pool.query(insertUsersQuery, [uid], (error,result)=>{
                     if(error)
                         res.end(error);
                     else{
@@ -104,7 +131,24 @@ app.post('/edituser', (req, res) => {
 });
 
 
-
+app.post('/showpassword', (req, res) => {
+    var uid = req.body.uid;
+    var uname = req.body.uname;
+    var uemail = req.body.uemail;
+    var values=[uid, uname, uemail];
+    if(uid && uname && uemail){
+        pool.query(`SELECT * from backpack where uid=$1 AND uemail=$2 AND uname=$3`, values, (error, result)=>{
+            if(error)
+                res.end(error);
+            else if(!result||!result.rows[0]){
+                res.send(`INFORMAION is not correct!`);
+            }
+            else{
+                res.send(result.rows[0].upassword);
+            }
+        })
+    }
+});
 
 
 
