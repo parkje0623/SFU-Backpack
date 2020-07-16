73417e29dc7184d5212c3c0586b98f233e6e4c04
Jieung Park <parkje0623@gmail.com>
2020-07-15 18:36:41 -0700

    forgot to change server

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ var pool;
 //user database access
 pool = new Pool({
     //connectionString:'postgres://postgres:SFU716!!qusrlgus@localhost/users' //-for keenan
-    connectionString:'postgres://postgres:cmpt276@localhost/postgres' //- for Jieung
-    //connectionString:process.env.DATABASE_URL
+    //connectionString:'postgres://postgres:cmpt276@localhost/postgres' //- for Jieung
+    connectionString:process.env.DATABASE_URL
 })
 
 //login session access
@@ -29,8 +29,8 @@ app.use(session({
     store: new Psession({
 
         //conString:'postgres://postgres:SFU716!!qusrlgus@localhost/postgres'
-        //conString: process.env.DATABASE_URL
-        conString:'postgres://postgres:cmpt276@localhost/postgres'
+        conString: process.env.DATABASE_URL
+        //conString:'postgres://postgres:cmpt276@localhost/postgres'
 
     }),
     secret: '!@SDF$@#SDF',
