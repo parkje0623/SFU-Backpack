6b98f64451027edd13ed5fa99cc691210ba56f95
Khoa Au <ddau@sfu.ca>
2020-07-19 14:43:21 -0700

    testing

--- index.js
+++ index.js
@@ -572,7 +572,7 @@ app.post("/upload", function (req, res) { // async function here
         })
       } else {
 
-        gepcpder.geocode(req.body.location, function (err, data){
+        geocoder.geocode(req.body.location, function (err, data){
           if (err || !data.length) {
             req.flash('error', 'Invalid address');
             return res.redirect('back')
