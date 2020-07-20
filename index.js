6480aa31f5066ffbc24b3adc422c233ede45985c
bgh716 <bgh716@naver.com>
2020-07-20 15:50:34 -0600

    forgot to change server..

--- index.js
+++ index.js
@@ -29,9 +29,9 @@ var geocoder = NodeGeocoder(options); /// google map geocoding
 
 //user database access
 pool = new Pool({
-  connectionString:'postgres://postgres:SFU716!!qusrlgus@localhost/users' //-for keenan
+  //connectionString:'postgres://postgres:SFU716!!qusrlgus@localhost/users' //-for keenan
   //connectionString:'postgres://postgres:cmpt276@localhost/postgres' //- for Jieung
-  //connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL,
 })
 
 //login session access
@@ -39,8 +39,8 @@ var app = express()
 app.use(
   session({
     store: new Psession({
-      conString:'postgres://postgres:SFU716!!qusrlgus@localhost/postgres'
-      //conString: process.env.DATABASE_URL,
+      //conString:'postgres://postgres:SFU716!!qusrlgus@localhost/postgres'
+      conString: process.env.DATABASE_URL,
       //conString:'postgres://postgres:cmpt276@localhost/postgres'
     }),
     secret: "!@SDF$@#SDF",
