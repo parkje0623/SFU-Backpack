a766cbb656261e0f0f690b39585735a5e57476f4
Jieung Park <parkje0623@gmail.com>
2020-07-22 20:46:07 -0700

    fixing reviews

--- index.js
+++ index.js
@@ -179,6 +179,76 @@ app.get("/select_page/:id", (req, res) => {
   }
 })
 
+
+//Posts the review written by the buyer
+app.post("/post_review", (req, res) => {
+  var uid = req.session.ID;
+  var sellerid = req.body.sellerID;
+  var review = req.body.review;
+  var postID = req.body.postID;
+  // current date + time
+  var date_ob = new Date();
+  var date = ("0" + date_ob.getDate()).slice(-2);
+  var month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+  var year = date_ob.getFullYear();
+  var hours = date_ob.getHours();
+  var minutes = date_ob.getMinutes();
+  var seconds = date_ob.getSeconds();
+  var timestamp = year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds;
+
+  var values = [timestamp, uid, sellerid, review];
+  var uidOnly = [sellerid];
+  var post_number = [postID];
+  if (uid && sellerid && review) {
+    //Inserting the review written to the database
+    pool.query(`INSERT INTO review (date, written_user, about_user, description) VALUES ($1, $2, $3, $4)`, values, (error, result)=>{
+      if (error)
+        res.end(error)
+      var backTo = "/select_page/" + postID;
+      res.redirect(backTo);
+    });
+  }
+})
+
+//This page allows user to view what reviews he/she got from other users, and what reviews user haven written to others
+app.get('/reviewpage', (req, res) => {
+  var uid = req.session.ID;
+  var value = [uid];
+
+  // This is login and logout checking functino
+  if (isLogedin(req, res)) {
+    //Selects all the reviews that were written by the current user
+    pool.query(`SELECT * FROM review WHERE written_user=$1`, value, (error, result) => {
+      if (error)
+        res.end(error)
+      var my_reviews = result.rows;
+      //Selects all the reviews that were written to the current user
+      pool.query(`SELECT * FROM review WHERE about_user=$1`, value, (error, result) => {
+        if (error)
+          res.end(error)
+        var other_reviews = result.rows;
+        if (req.session.ID.trim() == "admin") {
+          res.render("pages/reviews", {
+            my_reviews, other_reviews,
+            uname: req.session.displayName,
+            admin: true,
+
+          })
+        } else {
+          res.render("pages/reviews", {
+            my_reviews, other_reviews,
+              uname: req.session.displayName,
+              admin: false,
+            })
+          }
+        });
+      });
+    } else {
+        //Redirects to the select page
+        res.redirect("login")
+      }
+})
+
 app.get("/login", (req, res) => {
   res.render("pages/login", {})
 })
@@ -669,13 +739,13 @@ app.post("/report", (req, res) => {
     pool.query(getEmailQuery, (error, result) => {
       if (error) {
         res.end(error)
-      } 
+      }
       else if (!result || !result.rows[0]) {
         res.render("pages/reportUser", {
           msg: "INFORMAION about the User ID is not correct!",
         })
-      }    
-    }) 
+      }
+    })
     var getEmailQuery = "SELECT * FROM backpack WHERE uid='" + uid + "'"
     pool.query(getEmailQuery, (error, result) => {
       if (error) {
@@ -712,7 +782,7 @@ app.post("/report", (req, res) => {
           res.render("pages/reportUser", { msg: "Report has been sent" })
         })
       }
-    })                 
+    })
 });
 
 
@@ -795,7 +865,7 @@ app.get("/buy", (req, res) => {
           uname: req.session.displayName,
           admin: true,
         })
-        
+
       } else {
         res.render("pages/buyingpage", {
           results,
