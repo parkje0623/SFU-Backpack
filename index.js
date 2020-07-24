4a26ba71fbf62fe11571e1bd435679d0435c6ecb
bgh716 <bgh716@naver.com>
2020-07-23 18:23:37 -0600

    pulling

--- index.js
+++ index.js
@@ -29,9 +29,9 @@ var geocoder = NodeGeocoder(options); /// google map geocoding
 
 //user database access
 pool = new Pool({
-  //connectionString:'postgres://postgres:SFU716!!qusrlgus@localhost/users' //-for keenan
+  connectionString:'postgres://postgres:SFU716!!qusrlgus@localhost/users' //-for keenan
   //connectionString:'postgres://postgres:cmpt276@localhost/postgres' //- for Jieung
-  connectionString: process.env.DATABASE_URL,
+  //connectionString: process.env.DATABASE_URL,
 })
 
 //login session access
@@ -39,8 +39,8 @@ var app = express()
 app.use(
   session({
     store: new Psession({
-      //conString:'postgres://postgres:SFU716!!qusrlgus@localhost/postgres'
-      conString: process.env.DATABASE_URL,
+      conString:'postgres://postgres:SFU716!!qusrlgus@localhost/postgres'
+      //conString: process.env.DATABASE_URL,
       //conString:'postgres://postgres:cmpt276@localhost/postgres'
     }),
     secret: "!@SDF$@#SDF",
@@ -1047,3 +1047,5 @@ app.get('/search', function(req, res) {
 
 
 server.listen(PORT, () => console.log(`Listening on ${PORT}`))
+
+module.exports = app;
