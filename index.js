b25d0f95f71af142d2307c08caa429210cb8b801
bgh716 <bgh716@naver.com>
2020-07-31 13:08:46 -0600

    chatting room head

--- index.js
+++ index.js
@@ -1037,6 +1037,12 @@ app.post("/chat", (req, res)=> {
                 if(error){
                     res.end(error);
                 }
+                if (req.session.ID.trim() == "admin") {
+                    admin=true;
+                }
+                else{
+                    admin=false;
+                }
 
                 if (!result || !result.rows[0]) {
                     res.render("pages/chat",{uname: req.session.displayName, db:false, receiver:receiver, sender:req.session.ID});
