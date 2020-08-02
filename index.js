b2ad068bcaab4fba1c748fefdb0424207b4ca49f
KHAI <wnwkr13@gmail.com>
2020-08-02 22:20:26 +0900

    design added

--- index.js
+++ index.js
@@ -31,9 +31,10 @@ var geocoder = NodeGeocoder(options); /// google map geocoding
 //user database access
 pool = new Pool({
   //connectionString:'postgres://postgres:SFU716!!qusrlgus@localhost/users' //-for keenan
-  //connectionString:'postgres://postgres:cmpt276@localhost/postgres' //- for Jieung
+  connectionString:'postgres://postgres:@localhost/postgres' //- for Jieung
   // connectionString: "postgres://postgres:khoakhung@localhost/sfupb",
-  connectionString: process.env.DATABASE_URL,
+  // connectionString: "postgres://postgres:@localhost/postgres"
+  // connectionString: process.env.DATABASE_URL,
 
 })
 
@@ -43,9 +44,10 @@ app.use(
   session({
     store: new Psession({
       //conString:'postgres://postgres:SFU716!!qusrlgus@localhost/postgres'
-      conString: process.env.DATABASE_URL,
+      // conString: process.env.DATABASE_URL,
       //conString:'postgres://postgres:cmpt276@localhost/postgres'
       // conString: "postgres://postgres:khoakhung@localhost/postgres",
+      conString: "postgres://postgres:@localhost/postgres", //kai
     }),
     secret: "!@SDF$@#SDF",
     resave: false,
@@ -87,6 +89,8 @@ app.get("/mainpage", (req, res) => {
 app.get("/signUp", (req, res) => {
   res.render("pages/signUp")
 })
+
+
 
 //path to find pw page
 app.get("/find_pw", (req, res) => {
