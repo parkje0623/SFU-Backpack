7401d0124dbd6464991728b03012fe90b16b8f6f
Jieung Park <parkje0623@gmail.com>
2020-09-18 07:09:22 -0700

    geocoder

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ var NodeGeocoder = require('node-geocoder');   // map
 var options = {
   provider: 'google',
   httpAdapter: 'https',
-  apiKey: "process.env.GEOCODER_API_KEY",
+  apiKey: "AIzaSyCtOdXVisgfJwqevIlmYAHcH8I9EZ5wzRE",
   formatter: null
 };
 
@@ -996,8 +996,7 @@ app.post("/upload", function (req, res) { // async function here
           msg: "Error: No File Selected!",
         })
       } else {
-        //req.body.location
-        geocoder.geocode('Maple Ridge BC Canada', (err, data) => {
+        geocoder.geocode(req.body.location, (err, data) => {
           if (err || !data.length) {
             return res.render("pages/imageUpload", {
               // if no file was selected
